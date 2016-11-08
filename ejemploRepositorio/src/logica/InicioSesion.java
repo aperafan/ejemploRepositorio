f6d84dca60a91e3f0e7fa3608f41ba116c7f2f5c
aperafan <>
2016-11-08 09:39:56 -0500

    version Arley

---
+++ ejemploRepositorio/src/logica/InicioSesion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author ARLEY PERAFAN
+ */
+public class InicioSesion {
+   //System.out.println(" esta es el area de trabajo del proyecto ejemplo");
+    
+}
